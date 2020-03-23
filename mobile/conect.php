fc62cad356f83f1e6eff4488915f1a878229925a
mateus <mateus1908.schwede@gmail.com>
2020-03-23 13:55:03 -0300

    Padronização

--- mobile/conect.php
+++
@@ -1,3 +0,0 @@
-<?
-$db = new PDO('mysql:host=localhost;dbname=oficina;charset=utf8','root','');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- mobile/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['nomeLogin']);
-unset($_SESSION['senhaLogin']);
-session_destroy();
-header("location: index.php");
-?>
